3c6767f6e04c3e8987644da1ea3326025826f2ae
Liam <droidology@allianceofdroids.org.au>
2019-07-05 14:35:01 +0800

    added code from tutorial part 2 of the roll a ball

--- Roll-A-Ball Project/Game Files/July 2019/July 5/Roll a ball/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball Project/Game Files/July 2019/July 5/Roll a ball/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
     }
 }
