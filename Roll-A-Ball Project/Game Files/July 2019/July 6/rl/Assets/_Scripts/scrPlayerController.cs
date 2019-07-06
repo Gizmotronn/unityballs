b6836ae68a7c87d89c6496facfd944f9d421e756
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-07-06 16:45:50 +0800

    fixed script for player movement

---
+++ Roll-A-Ball Project/Game Files/July 2019/July 6/rl/Assets/_Scripts/scrPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrPlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement);
+    }
+}
