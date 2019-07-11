0b096cd3b3301bbbe97e3e43f5200add0da7c8d1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-07-11 15:59:00 +0800

    Completed tutorial #2 from Unity - Intro to Scripting
    Video link: https://learn.unity.com/tutorial/variables-and-functions?courseId=5c61706dedbc2a324a9b022d&projectId=5c8920b4edbc2a113b6bc26a#
    
    ACORD Forum Link: http://allianceofdroids.org.au/aod/jedi-order/programmers/forum/topic/unity-introduction-to-scripting/#post-58796

---
+++ Intro To Scripting/Assets/Scripts/CubeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColor : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
