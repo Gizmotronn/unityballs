fa0656ed53886f7bdae126147c96fa5e83cbb575
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-07-23 12:52:25 +0800

    ACORD - Asteroid Dodgers Build 4
    The ship can now move!
    
    Build 5 coming soon

---
+++ Asteroid Dodgers/Builds/Build 4 - 22.7.19/rl/Assets/_Scripts/ShipMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal") * 5;
+        float v = Input.GetAxis("Vertical") * 5;
+
+        Vector3 vel = rb.velocity;
+
+        vel.x = h;
+        vel.z = v;
+        rb.velocity = vel;
+
+    }
+
+}
