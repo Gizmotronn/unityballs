bf05682f9800a43dbe565cf47959ec209dffe5e4
Liam Arbuckle <droidology@allianceofdroids.org.au>
2019-09-09 17:58:22 +0800

    Commit described in link below
    https://github.com/acord-robotics/stellarios/issues/36
    
    Also store Stellarios in the "stellarios-builds" branch
    Unlock Github Pro for Acord-Robotics corporation, so we can make the Stellarios repo private, fork it and back and forth with gitlab.
    
    Stellarios will be built into Unity, with all the assets stored in Unity, like in the image added at root:

---
+++ Asteroid Dodgers/rl/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+     public void MainMenu ()
+    {
+        SceneManager.LoadScene("MainMenu"); // Transitions the scene to the scene called "MainMenu" (from the options scene) which is the main menu
+    }
+   
+}
